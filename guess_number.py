e1934ed55b531f46b001a882f92bbd607af439d3
codepopo <epopo543@gmail.com>
2018-11-06 16:46:45 +0800

    uesr choice

--- guess_number.py
+++ guess_number.py
@@ -2,14 +2,19 @@
 
 
 
-
 import random
-r = random.randint(1,100)
+print ("請輸入兩個數字,來猜猜範圍內的隨機數值")  #讓使用者自己決定猜的範圍
+a = input("請輸入第一個數字: ")
+b = input('請輸入第二個數字: ')
+a = int(a)
+b = int(b)
+r = random.randint(a,b)
 time = 0
+print('你選定的範圍是',a, '到', b)
 while True :
-	x = input('請輸入一個數字 1-100:', )
+	x = input('請輸入一個數字:', )
 	x = int(x)	
-	time = time + 1
+	time += 1       #與time = time +1 是相同意思
 	if r == x :
 		print('正確答案是',r,'你答對了呢!')
 		print('你總共猜了',time,'次')
