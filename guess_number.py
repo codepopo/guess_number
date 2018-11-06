5658f21e031e62fa2f978ff58808594b282d00b0
codepopo <epopo543@gmail.com>
2018-11-06 16:22:11 +0800

    how many times guess

--- guess_number.py
+++ guess_number.py
@@ -5,11 +5,14 @@
 
 import random
 r = random.randint(1,100)
+time = 0
 while True :
 	x = input('請輸入一個數字 1-100:', )
 	x = int(x)	
+	time = time + 1
 	if r == x :
 		print('正確答案是',r,'你答對了呢!')
+		print('你總共猜了',time,'次')
 		break
 	elif x < r :
 		print('數字太小')
